7032b848311838f90119ff7f81e542426ed3f6c8
anouri <ultraadri@free.fr>
2023-04-24 13:06:24 +0200

    [Init] /Part final/ Finish Raylib ECS engine

---
+++ game/game/Entity.hpp
@@ -0,0 +1,16 @@
+#ifndef GAMEIMPL_ENTITY_HPP
+#define GAMEIMPL_ENTITY_HPP
+
+#include <array>
+#include "engine/Defines.hpp"
+
+constexpr i32 MAX_COMPONENTS = 3;
+
+struct Entity 
+{
+	explicit Entity(u64 idP) : id{ idP } {}
+	u64 id;
+	std::array<i32, MAX_COMPONENTS> components{ -1, -1, -1 };
+};
+
+#endif //GAMEIMPL_ENTITY_HPP
